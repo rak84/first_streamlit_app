257773c9e56aaea7c17a064ac48a210ac0e839d5
rak84 <121898830+rak84@users.noreply.github.com>
2023-04-09 11:32:52 -0500

    Update streamlit_app.py
    function

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 streamlit.dataframe(fruits_to_show) 
 
+#Create Function
+def get_fruityvice_data(this_fruit_choce):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+    return fruityvice_normalized
 
+ #API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_choice)
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+    else:
+      back_from_function = get_fruityvice_data(fruit_choice)    
+      streamlit.dataframe(back_from_function)
+    
+except URLError as e:
+  streamlit.error()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
